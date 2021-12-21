ab747bf68d302410133822fc874d9dc97197ecdf
cj3an <cj3an@DESKTOP-3LMP021.home>
2021-12-20 18:30:07 -0800

    ChatApp.java

--- src/_02_Chat_Application/ChatApp.java
+++ src/_02_Chat_Application/ChatApp.java
@@ -1,5 +1,7 @@
 package _02_Chat_Application;
 
+import javax.swing.JOptionPane;
+
 import _00_Click_Chat.networking.Client;
 import _00_Click_Chat.networking.Server;
 
@@ -10,6 +12,25 @@ import _00_Click_Chat.networking.Server;
 public class ChatApp {
 	Server server;
 	Client client; 
+	boolean messageSent;
+	
+	
+	public ChatApp(){
+		int response = JOptionPane.showConfirmDialog(null, "Would you like to host a connection?", "Chat", JOptionPane.YES_NO_OPTION);
+		if(response == JOptionPane.YES_OPTION){
+			server = new Server(8080);
+			JOptionPane.showMessageDialog(null, "Server started at: " + server.getIPAddress() + "\nPort: " + server.getPort());
+			
+			
+		}else{
+			String ipSTR = JOptionPane.showInputDialog("Enter IP address: ");
+			String portSTR = JOptionPane.showInputDialog("Enter Port Number: ");
+			int port = Integer.parseInt(portSTR);
+			client = new Client(ipSTR, port);
+			
+		}
+		
+	}
 	
 	
 }
