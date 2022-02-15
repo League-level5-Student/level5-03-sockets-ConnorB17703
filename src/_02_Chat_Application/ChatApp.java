91ce512787949ff758d24de7ed0d256e4236697d
cj3an <cj3an@DESKTOP-3LMP021.home>
2022-02-14 18:27:15 -0800

    ChatApp.java

--- src/_02_Chat_Application/ChatApp.java
+++ src/_02_Chat_Application/ChatApp.java
@@ -1,6 +1,13 @@
 package _02_Chat_Application;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import _00_Click_Chat.networking.Client;
 import _00_Click_Chat.networking.Server;
@@ -9,29 +16,48 @@ import _00_Click_Chat.networking.Server;
  * Using the Click_Chat example, write an application that allows a server computer to chat with a client computer.
  */
 
-public class ChatApp {
+public class ChatApp implements ActionListener {
 	Server server;
 	String severInput;
 	Client client; 
 	String clientInput;
 	boolean messageSent;
 	
+	JFrame frame;
+	JPanel panel;
+	JButton button;
+	JTextField textField;
+	
 public static void main(String[] args) {
 	new ChatApp(); 
-	//From last time you fixed the connecting issue and both the server and client are able to communicate with each other
-	//You need to fix the user interface so that its not as "junky" 
-	//Like create a chat room using the console or JFrames
-	
+	//you were able to create a window for both the server and client. Now you need to write code to clean up the format
 	
 }
 	
 	public ChatApp(){
+		
+		frame = new JFrame("Chat App");
+		panel = new JPanel();
+		button = new JButton("Send Message"); 
+		textField = new JTextField();
+		
+		
+		frame.add(panel);
+		panel.add(button);
+		panel.add(textField);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(500, 500);
+		frame.setVisible(false);
+		
+		
 		int response = JOptionPane.showConfirmDialog(null, "Would you like to host a connection?", "Chat", JOptionPane.YES_NO_OPTION);
 		if(response == JOptionPane.YES_OPTION){
+			frame.setVisible(true);
+			frame.setTitle("Chat App: Server");
+			
 			server = new Server(8080);
 			server.start();
-			//System.out.println("Sever created");
-			//JOptionPane.showMessageDialog(null, "Server started at: " + server.getIPAddress() + "\nPort: " + server.getPort());
 			if(messageSent == false){
 				severInput = JOptionPane.showInputDialog("Send a message:");
 				server.sendMess(severInput);
@@ -47,12 +73,15 @@ public static void main(String[] args) {
 		}else{
 			String ipSTR = JOptionPane.showInputDialog("Enter IP address: ");
 			String portSTR = JOptionPane.showInputDialog("Enter Port Number: ");
+			frame.setVisible(true);
+			frame.setTitle("Chat App: Client");
+			
 			int port = Integer.parseInt(portSTR);
 			client = new Client(ipSTR, port);
 			client.start();
 			//System.out.println("Client created");
 			if(messageSent == false){
-				clientInput = JOptionPane.showInputDialog("Send a message:");
+				//clientInput = JOptionPane.showInputDialog("Send a message:");
 				client.sendMess(clientInput);
 				messageSent = true;
 			}else{
@@ -64,6 +93,12 @@ public static void main(String[] args) {
 			
 			
 		}
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
 		
 	}
 	
